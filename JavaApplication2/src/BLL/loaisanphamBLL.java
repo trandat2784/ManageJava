cff3495b529ba72bafce691d2c5f7ea15bd72b6e
subnauticaearly@gmail.com <subnauticaearly@gmail.com>
2025-06-06 07:36:52 +0700

    fix add dloaisanpham

--- JavaApplication2/src/BLL/loaisanphamBLL.java
+++ JavaApplication2/src/BLL/loaisanphamBLL.java
@@ -4,85 +4,60 @@
  */
 package BLL;
 
-import Config.ConnectDB;
 import DAL.LoaiSanPham;
+import Config.ConnectDB;
+import DAL.SanPham;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Vector;
+import javax.swing.DefaultListModel;
 
 /**
  *
- * @author Windows
+ * @author Ngoc
  */
-public class loaisanphamBLL extends ConnectDB{
-    public List<LoaiSanPham> getAllCategory() throws SQLException {
-        ConnectDB connection= new ConnectDB();
-        Connection conn = connection.getConnection();
-        if (conn == null) {
-          System.out.println("❌ Kết nối thất bại, conn = null tại HomeDAO");
-         return new ArrayList<>(); // tránh lỗi tiếp theo
-}
-        String sql = "SELECT * FROM loaisanpham";
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(sql);
-        List<LoaiSanPham> categorys = new ArrayList<>();
+public class LoaiSanPhamBLL extends ConnectDB {
+    
+   public Vector<LoaiSanPham> docdanhmuc() {
+        Vector<LoaiSanPham> vec = new Vector<LoaiSanPham>();
+        try {
+            Connection con = ConnectDB.getConnection();
+            String sql = "select * from loaisanpham";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
 
-        while (rs.next()) {
-            LoaiSanPham category = new LoaiSanPham(
-                rs.getInt("maloai"),
-                rs.getString("tenloai")
-                
-            );
-            categorys.add(category);
+            while (rs.next()) {
+                LoaiSanPham dm = new LoaiSanPham();
+                dm.setMaLoai(rs.getInt(1));
+                dm.setTenLoai(rs.getString(2));
+                vec.add(dm);
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        System.out.println("categorys size"+categorys.size());
-        rs.close();
-        stmt.close();
-        conn.close();
-        return categorys;
-    }
-    
-    public boolean insertCategory(LoaiSanPham p) throws SQLException {
-        ConnectDB connection= new ConnectDB();
-        Connection conn = connection.getConnection();
-        String sql = "INSERT INTO loaisanpham  (tenloai) VALUES (  ?)";
-        PreparedStatement ps = conn.prepareStatement(sql);
-        ps.setString(1, p.getTenLoai());
-        int rows = ps.executeUpdate();
-        ps.close();
-        return rows > 0;
-        
-    }
-    public boolean updateCategory(LoaiSanPham p) throws SQLException {
-        ConnectDB connection= new ConnectDB();
-        Connection conn = connection.getConnection();
-        String sql = "Update loaisanpham Set tenloai=? where maloai=?";
-        PreparedStatement ps = conn.prepareStatement(sql);
-        ps.setInt(2, p.getMaLoai());
-        ps.setString(1, p.getTenLoai());
-        int rows = ps.executeUpdate();
-        ps.close();
-        return rows > 0;
-    }
-     public boolean deleteCategory(int maloai) throws SQLException {
-        ConnectDB connection= new ConnectDB();
-        Connection conn = connection.getConnection();
-         String sql = "DELETE FROM loaisanpham WHERE maloai=?";
-        PreparedStatement ps = conn.prepareStatement(sql);
-        ps.setInt(1, maloai);
-        int rows = ps.executeUpdate();
-        ps.close();
-        conn.close();
-        return rows > 0;
-     }
-     
 
-    
-    
-    
+        return vec;
+    }
+   
+   public String timMaDanhMucTheoTen(String tenloai) {
+       Connection con = ConnectDB.getConnection();
+    String ma = null;
+    try {
+        String sql = "SELECT maloai FROM loaisanpham WHERE tenloai = ?";
+        PreparedStatement pstm = con.prepareStatement(sql);
+        pstm.setString(1, tenloai);
+        ResultSet rs = pstm.executeQuery();
+        if (rs.next()) {
+            ma = rs.getString("maloai");
+        }
+    } catch (Exception ex) {
+        ex.printStackTrace();
+    }
+    return ma;
+}
 }
